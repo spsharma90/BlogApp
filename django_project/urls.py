9b0b596450e891685369360b49136c67d0e77c6e
spsharma90 <48118234+spsharma90@users.noreply.github.com>
2024-09-27 17:06:49 +0530

    Update urls.py

--- django_project/urls.py
+++ django_project/urls.py
@@ -20,6 +20,8 @@ from users import views as user_views
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -40,10 +42,13 @@ urlpatterns = [
         auth_views.PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"),
         name='password_reset_complete'),
     path('',include('blog.urls')),
-
+    
 
 ]
 
+urlpatterns += staticfiles_urlpatterns()
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
